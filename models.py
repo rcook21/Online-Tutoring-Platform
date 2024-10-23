5aac75c2693099e20d5c2eb485270d2a1df29e4d
rcook21 <135378048+rcook21@users.noreply.github.com>
2024-10-23 10:24:28 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class User(models.Model):
+  username = models.CharField(max_length = 100)
+  password = models.CharField(max_length = 100)
+
+  def __str__(self):
+    return self.username
 # Create your models here.
