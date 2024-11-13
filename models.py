c801ee27cd814acfbbd8f51ccc40717b17610c57
Marcus Pham <110352874+MarcusPham02@users.noreply.github.com>
2024-11-13 15:21:42 -0500

    Changes

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
+# myApp/models.py
 from django.db import models
 
-class User(models.Model):
-  username = models.CharField(max_length = 100)
-  password = models.CharField(max_length = 100)
 
-  def __str__(self):
-    return self.username
-# Create your models here.
+
+
+
+
+
--- views.py
+++ views.py
@@ -1,9 +1,75 @@
-from django.http import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
-def page (request,*args, **kwargs):
-    return HttpResponse("<h1> Hello World </h1>")
+
+# Define a view function for the home page
+def home(request):
+    return render(request, 'myApp/home.html')
+
+# Define a view function for the login page
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        if not User.objects.filter(username=username).exists():
+            messages.error(request, 'Invalid Username')
+            return redirect('/login/')
+        
+        user = authenticate(username=username, password=password)
+        
+        if user is None:
+            messages.error(request, "Invalid Password")
+            return redirect('/login/')
+        else:
+            login(request, user)
+            return redirect('/course/')
+    
+    return render(request, 'myApp/login.html')
+
+# Define a view function for the registration page
+def register_page(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = User.objects.filter(username=username)
+        
+        if user.exists():
+            messages.info(request, "Username already taken!")
+            return redirect('/register/')
+        
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username
+        )
+        user.set_password(password)
+        user.save()
+        
+        messages.info(request, "Account created successfully! Please log in.")
+        return redirect('/login/')
+    
+    return render(request, 'myApp/register.html')
+
+# Protect the course page with login_required
+@login_required
+def course(request):
+    return render(request, 'myApp/main.html')
+
+
+def join(request):
+
+
+    return render(request,'myApp/join.html')
+
+
 
 
 
